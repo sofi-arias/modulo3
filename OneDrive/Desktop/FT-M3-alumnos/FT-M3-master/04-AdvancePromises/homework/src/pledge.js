b114a72a2589eacdcaf0f57568a7d3a470889173
sofia-arias <sofi_100_15@hotmail.com>
2022-11-04 11:15:11 -0300

    ok

--- OneDrive/Desktop/FT-M3-alumnos/FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ OneDrive/Desktop/FT-M3-alumnos/FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -31,17 +31,49 @@ $Promise.prototype._internalReject = function(value) {
 $Promise.prototype.then = function(successCb, errorCb) {
     if(typeof successCb !== 'function') successCb = false;
     if(typeof errorCb !== 'function') errorCb = false;
-    this._handlerGroups.push({successCb, errorCb});
+    const downstreamPromise = new $Promise(function(){})
+    this._handlerGroups.push({successCb, errorCb, downstreamPromise});
     if(this._state !== 'pending') this._callHandlers();
+    return downstreamPromise;
+
 };
 
 $Promise.prototype._callHandlers = function(){
     while(this._handlerGroups.length > 0){
         let current = this._handlerGroups.shift();
         if(this._state ==='fulfilled'){
-            current.successCb && current.successCb(this._value);
+            if(!current.successCb){
+                current.downstreamPromise._internalResolve(this._value);
+            }else{
+                try{
+                    const result = current.successCb(this._value);
+                    if(result instanceof $Promise){
+                        result.then(value => current.downstreamPromise._internalResolve(value),
+                        err => current.downstreamPromise._internalReject(err));
+                    }else{
+                        current.downstreamPromise._internalResolve(result);
+                    } 
+                } catch(e){
+                    current.downstreamPromise._internalReject(e);
+                }
+            }
+            //current.successCb && current.successCb(this._value);
         }else if (this._state === 'rejected'){
-            current.errorCb && current.errorCb(this._value);
+            if(!current.errorCb){
+                current.downstreamPromise._internalReject(this._value);
+            }else{
+                try{
+                    const result = current.errorCb(this._value);
+                    if(result instanceof $Promise){
+                        result.then(value => current.downstreamPromise._internalResolve(value),
+                        err => current.downstreamPromise._internalReject(err));
+                    }else{
+                        current.downstreamPromise._internalResolve(result);
+                    }
+                }catch(e){
+                    current.downstreamPromise._internalReject(e);
+                }
+            }
         }
     }
 };
